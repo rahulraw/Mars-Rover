bb9e6a07b92029dae62cc8ab10f8e865a3a2d14f
Chunshang Li <jack.chunshang.li@gmail.com>
2015-01-30 23:42:39 -0500

    cleaned up the arm control code

--- Software/Incubation/Arm/arm_control.py
+++ Software/Incubation/Arm/arm_control.py
@@ -15,7 +15,41 @@ class JrkArm:
         self.jrk = jrkController
         self.dir = JrkArm.STOP
         self.prevDir = JrkArm.STOP
-        self.stopTarget = 0
+        self.stopTarget = self.jrk.jrkGetFeedBack()
+
+    def update(self, ctl_val):
+        if ctl_val > 45:
+            arm_dir = JrkArm.EXTEND
+        elif ctl_val < -45:
+            arm_dir = JrkArm.RETRACT
+        else:
+            arm_dir = JrkArm.STOP
+
+        if arm_dir != self.dir:
+            self.prevDir = self.dir
+            self.dir = arm_dir
+
+            if self.dir == JrkArm.STOP:
+                self.stopTarget = self.jrk.jrkGetFeedBack()
+
+    def run(self):
+
+        feedback = self.jrk.jrkGetFeedBack()
+
+        if self.dir == JrkArm.EXTEND or self.dir == JrkArm.RETRACT:
+            
+            target =  feedback + self.dir * JrkArm.JRK_TARGET_OFFSET
+
+            if target < JrkArm.JRK_TARGET_MIN:
+                target = JrkArm.JRK_TARGET_MIN
+            elif target > JrkArm.JRK_TARGET_MAX:
+                target = JrkArm.JRK_TARGET_MAX
+
+        else:
+            target = self.stopTarget
+
+        print("Dir:%2d   Feedback:%4d   Target:%4d" % (self.dir, feedback, target))
+        self.jrk.jrkSetTarget(target)
 
 
 class ArmControl:
@@ -34,41 +68,7 @@ class ArmControl:
 
     def callback(self, data):
 
-        jrk1_dir_raw = data.data[0]
-
-        if jrk1_dir_raw > 45:
-            arm_dir = JrkArm.EXTEND
-        elif jrk1_dir_raw < -45:
-            arm_dir = JrkArm.RETRACT
-        else:
-            arm_dir = JrkArm.STOP
-
-        if arm_dir != self.jrkArm1.dir:
-            self.jrkArm1.prevDir = self.jrkArm1.dir
-            self.jrkArm1.dir = arm_dir
-
-            if self.jrkArm1.dir == JrkArm.STOP:
-                self.jrkArm1.stopTarget = self.jrkArm1.jrk.jrkGetFeedBack()
-
-
-    def run_jrk(self, jrkArm):
-
-        feedback = jrkArm.jrk.jrkGetFeedBack()
-
-        if jrkArm.dir == JrkArm.EXTEND or jrkArm.dir == JrkArm.RETRACT:
-            
-            target =  feedback + jrkArm.dir * JrkArm.JRK_TARGET_OFFSET
-
-            if target < JrkArm.JRK_TARGET_MIN:
-                target = JrkArm.JRK_TARGET_MIN
-            elif target > JrkArm.JRK_TARGET_MAX:
-                target = JrkArm.JRK_TARGET_MAX
-
-        else:
-            target = jrkArm.stopTarget
-
-        print("Dir:%2d   Feedback:%4d   Target:%4d" % (jrkArm.dir, feedback, target))
-        jrkArm.jrk.jrkSetTarget(target)
+        self.jrkArm1.update(data.data[0])
 
     def start(self):
 
@@ -76,7 +76,7 @@ class ArmControl:
         
         while not rospy.is_shutdown():
 
-            self.run_jrk(self.jrkArm1)
+            self.jrkArm1.run()
             self.rate.sleep()
 
 if __name__=='__main__':
