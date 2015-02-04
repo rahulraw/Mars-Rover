4bdfb25abfba0d5423f1783774233ad3072cf113
sean <sean@sean-ThinkPad-W520.(none)>
2015-02-03 19:40:15 -0500

    Got the open loop control for arm working

--- Software/Incubation/Arm/arm_control.py
+++ Software/Incubation/Arm/arm_control.py
@@ -1,83 +1,62 @@
 import rospy
 import jrk
-from std_msgs.msg import Int16MultiArray
+from joystick_packages.msg import Controller
 
 class JrkArm:
 
-    STOP = 0
-    EXTEND = 1
-    RETRACT = -1
-    JRK_TARGET_OFFSET = 500
-    JRK_TARGET_MIN = 0
-    JRK_TARGET_MAX = 4095
+    JRK_TARGET_CONSTANT = 27
+    JRK_TARGET_MIN = 100
+    JRK_TARGET_MAX = 4000
 
-    def __init__(self, jrkController):
-        self.jrk = jrkController
-        self.dir = JrkArm.STOP
-        self.prevDir = JrkArm.STOP
-        self.stopTarget = 0
+    def __init__(self, node_id):
+        self.jrk = jrk.Jrk(node_id)
+        self.direction = 0
+        self.target = self.input_from_target(self.jrk.jrkGetFeedBack())
+
+    def contain(self, target):
+        return max(
+                self.JRK_TARGET_MIN, 
+                min(self.JRK_TARGET_MAX, target)
+            )
+
+    def input_from_target(self, target):
+        return self.contain(int(target * 1.142 - 88.782))
+    
+    def set_direction(self, direction):
+        self.direction = direction
+
+    def run(self):
+        self.target = self.contain(self.target + self.direction * self.JRK_TARGET_CONSTANT)
+        self.jrk.jrkSetTarget(self.target)
 
 
 class ArmControl:
-
     def __init__(self, topic = 'RCValues', node = "ArmControl"):
-
         self.topic = topic
         self.node = node
 
-        self.jrkArm1 = JrkArm(jrk.Jrk("/dev/ttyACM0"))
+        self.arm1 = JrkArm("/dev/ttyACM0")
+        self.controller = Controller()
 
         rospy.init_node(self.node, anonymous=True)
-        rospy.Subscriber(self.topic, Int16MultiArray, self.callback)
-        self.rate = rospy.Rate(5)
-        #rospy.spin()
+        rospy.Subscriber(self.topic, Controller, self.callback)
+        self.rate = rospy.Rate(10)
+        
+    def get_direction(self, value):
+        return int(0 if not value else value / abs(value))
 
-    def callback(self, data):
-
-        jrk1_dir_raw = data.data[0]
-
-        if jrk1_dir_raw > 45:
-            arm_dir = JrkArm.EXTEND
-        elif jrk1_dir_raw < -45:
-            arm_dir = JrkArm.RETRACT
-        else:
-            arm_dir = JrkArm.STOP
-
-        if arm_dir != self.jrkArm1.dir:
-            self.jrkArm1.prevDir = self.jrkArm1.dir
-            self.jrkArm1.dir = arm_dir
-
-            if self.jrkArm1.dir == JrkArm.STOP:
-                self.jrkArm1.stopTarget = self.jrkArm1.jrk.jrkGetFeedBack()
-
-
-    def run_jrk(self, jrkArm):
-
-        feedback = jrkArm.jrk.jrkGetFeedBack()
-
-        if jrkArm.dir == JrkArm.EXTEND or jrkArm.dir == JrkArm.RETRACT:
-            
-            target =  feedback + jrkArm.dir * JrkArm.JRK_TARGET_OFFSET
-
-            if target < JrkArm.JRK_TARGET_MIN:
-                target = JrkArm.JRK_TARGET_MIN
-            elif target > JrkArm.JRK_TARGET_MAX:
-                target = JrkArm.JRK_TARGET_MAX
-
-        else:
-            target = jrkArm.stopTarget
-
-        print("Dir:%2d   Feedback:%4d   Target:%4d" % (jrkArm.dir, feedback, target))
-        jrkArm.jrk.jrkSetTarget(target)
+    def callback(self, controller):
+        self.arm1.set_direction(self.get_direction(controller.left_joy_y))
 
     def start(self):
 
-        self.jrkArm1.jrk.jrkGetErrorFlagsHalting()
+        self.arm1.jrk.jrkGetErrorFlagsHalting()
         
         while not rospy.is_shutdown():
 
-            self.run_jrk(self.jrkArm1)
+            self.arm1.run()
             self.rate.sleep()
+
 
 if __name__=='__main__':
     args = ["RCValues", "ArmControl"]
