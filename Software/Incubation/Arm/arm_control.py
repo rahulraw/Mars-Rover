1a32c847cdd270f373081c5b8416520484f40920
sean <sean@sean-ThinkPad-W520.(none)>
2015-02-03 20:05:46 -0500

    test

--- Software/Incubation/Arm/arm_control.py
+++ Software/Incubation/Arm/arm_control.py
@@ -1,33 +1,45 @@
+import math
 import rospy
 import jrk
 from joystick_packages.msg import Controller
 
 class JrkArm:
 
-    JRK_TARGET_CONSTANT = 27
     JRK_TARGET_MIN = 100
     JRK_TARGET_MAX = 4000
 
     def __init__(self, node_id):
         self.jrk = jrk.Jrk(node_id)
         self.direction = 0
+        self.stopped = True
         self.target = self.input_from_target(self.jrk.jrkGetFeedBack())
 
     def contain(self, target):
-        return max(
+        return int(math.floor(
+            max(
                 self.JRK_TARGET_MIN, 
                 min(self.JRK_TARGET_MAX, target)
             )
+        ))
+
+    def get_stop_target(self):
+        return self.contain(self.input_from_target(self.jrk.jrkGetFeedBack()))
 
     def input_from_target(self, target):
-        return self.contain(int(target * 1.142 - 88.782))
+        return self.contain(target * 1.142 - 88.782)
     
     def set_direction(self, direction):
         self.direction = direction
 
     def run(self):
-        self.target = self.contain(self.target + self.direction * self.JRK_TARGET_CONSTANT)
-        self.jrk.jrkSetTarget(self.target)
+
+        if not self.direction and not self.stopped:
+            self.stopped = True
+            self.jrk.jrkSetTarget(self.get_stop_target())
+        elif self.direction:
+            self.stopped = False
+            target = int(2000 + 2000 * self.direction)
+            self.jrk.jrkSetTarget(target)
 
 
 class ArmControl:
