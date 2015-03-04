27124db9c124a944251e8119c25ca82c2290c306
Chunshang Li <jack.chunshang.li@gmail.com>
2015-03-03 20:52:25 -0500

    started off on the ros stuff for camera

--- Software/Incubation/Arduino/CameraControl/CameraControl.ino
+++ Software/Incubation/Arduino/CameraControl/CameraControl.ino
@@ -1,4 +1,7 @@
-#include <Servo.h> 
+//#include <ArduinoHardware.h>
+#include <Servo.h>
+#include <ros.h>
+#include <std_msgs/Empty.h>
 
 //do a half half voltage divider w/ green=GND, red=+5 yellow=FB
 #define PIN_CAM_ROTATE  2//backward/forward controlled PWM
@@ -10,6 +13,14 @@
 
 Servo srvCamRot, srvCamPitch;
 
+ros::NodeHandle nh;
+
+void rosCallback(const std_msgs::Empty& msg){
+    
+}
+
+ros::Subscriber<std_msgs::Empty> rosSub("toggle_led", &rosCallback );
+
 void setup(){
     srvCamRot.attach(PIN_CAM_ROTATE);
     srvCamRot.writeMicroseconds(CAM_ROTATE_STOP);
@@ -17,15 +28,19 @@ void setup(){
     camRotateCCW();
 
     srvCamPitch.attach(PIN_CAM_PITCH);
-    
+    camPitch(125);
+
+
+    nh.initNode();
+    nh.subscribe(rosSub);
 }
 
 void loop(){
-    camRotateCCW();
-    camPitch(60);
+    // camRotateCCW();
+    camPitch(110);
     delay(1000);
-    camRotateCW();
-    camPitch(120);
+    // camRotateCW();
+    camPitch(140);
     delay(1000);
 }
 
