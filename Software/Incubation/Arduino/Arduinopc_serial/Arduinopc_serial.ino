d424211b380bfe7f5fffa478b39b8f0621b37334
marsrover <marsrover@marsrover-Aspire-3830TG.(none)>
2015-03-07 20:43:52 -0500

    fixed errors for Arduino code + implemented limit switch interrupts

--- Software/Incubation/Arduino/Arduinopc_serial/Arduinopc_serial.ino
+++ Software/Incubation/Arduino/Arduinopc_serial/Arduinopc_serial.ino
@@ -1,14 +1,14 @@
-/* 1st byte is topic 
-  
-*/
-
 #include <Servo.h>
 
 //Serial Initilization
 int bytes[] = { 1, 2, 1 };
-typedef void(*FunctionPointers)(char*);
-FunctionPointers FunctionPointer[] = { &shutOff, &panAndTilt, &homing };
 char data[2];
+typedef void(*FunctionPointers)(char*);
+FunctionPointers FunctionPointer[] = { &shutOff, &panAndTilt };//, &homing };
+
+//LimitChecker
+int limitA;
+
 
 //AutoShutDown
 int RelayPin;
@@ -23,7 +23,7 @@ int align_msg;
 
 //CameraControl
 //do a half half voltage divider w/ green=GND, red=+5 yellow=FB
-#define PIN_CAM_ROTATE  2//backward/forward controlled PWM
+#define PIN_CAM_ROTATE  4//backward/forward controlled PWM
 #define CAM_ROTATE_STOP 1354
 #define CAM_ROTATE_CW   CAM_ROTATE_STOP + 50
 #define CAM_ROTATE_CCW  CAM_ROTATE_STOP - 50
@@ -33,19 +33,19 @@ Servo srvCamRot, srvCamPitch;
 
 void shutOff(char * data)
 {
-  if ((int) data[0])
-  {
-    digitalWrite(RelayPin, LOW);
-  }
-  else 
-  {
-    digitalWrite(RelayPin, HIGH);
-  }
+    if ((int) data[0])
+    {
+        digitalWrite(RelayPin, LOW);
+    }
+    else 
+    {
+        digitalWrite(RelayPin, HIGH);
+    }
 }
 
 void panAndTilt(char * data)
 {
-  
+
 }
 
 //Camera functions
@@ -64,22 +64,24 @@ void camPitch(int angle){
 
 void homing(char * data)
 {
-  digitalWrite(13,digitalRead(SensorInput));
-  align_msg = (digitalRead(SensorInput));//Sends 1 if aligned 0 if not.
-  align_msg = 1;
-  delay(80);
- //write to PC 
+    digitalWrite(13,digitalRead(SensorInput));
+    align_msg = (digitalRead(SensorInput));//Sends 1 if aligned 0 if not.
+    align_msg = 1;
+    delay(80);
 }
 
 
-
 void setup() {
-       
+
     Serial.begin(9600);
-  //autoshutdown
+    attachInterrupt(0, limitCheck, CHANGE); // Attaching Int.0 = pin 2 -- Limit Switch
+    typedef void(*FunctionPointer)(char*);
+    FunctionPointer FunctionPointers[] = { &shutOff, &panAndTilt, &homing };
+
+    //autoshutdown
     pinMode(RelayPin, OUTPUT);  
-    
-  //Homing
+
+    //Homing
     //SensorInit(SensorInputFR,SensorOutputFR);
     //SensorInit(SensorInputFL,SensorOutputFL);
     //SensorInit(SensorInputBR,SensorOutputBR);
@@ -87,34 +89,58 @@ void setup() {
     pinMode(SensorInput, INPUT);
     pinMode(13, OUTPUT);
     
-  //CameraControl
+
+
+    //CameraControl
     srvCamRot.attach(PIN_CAM_ROTATE);
     srvCamRot.writeMicroseconds(CAM_ROTATE_STOP);
     camRotateCCW();
     srvCamPitch.attach(PIN_CAM_PITCH);
     camPitch(125);
-  
+
     //char data[2];
-    //typedef void(*FunctionPointers)(char*);
-    //FunctionPointer FunctionPointers[] = { &shutOff, &panAndTilt, &homing }
+}
+
+void loop() {
+
+    if (Serial.available() > 0) {
+
+        int topic = (int) Serial.read();
+
+        for (int i = 0; i < bytes[topic]; i++) {
+            data[i] = Serial.read();
+        }
+
+        handleSubscribers(topic, data);
+    }
+    delay(20);
 
 }
-  
-void loop() {
-        
-      if (Serial.available() > 0) {
-          
-        int topic = (int) Serial.read();
-        
-        for (int i = 0; i < bytes[topic]; i++) {
-          data[i] = Serial.read();
-        }
-        
-        handleSubscribers(topic, data);
-      }
+
+void limitCheck()
+
+{
+    //delay(2);
+    int newValue = digitalRead(2);
+     
+    if (limitA != newValue && newValue == 1){
+        Serial.print("New Value: ");
+        Serial.print(newValue);
+        Serial.print(" limitA: ");
+        Serial.println(limitA);      
+    }  
+
+    limitA = newValue;
 }
+
 
 
 void handleSubscribers(int topic, char * data) {
-  FunctionPointer[topic](data);
+    FunctionPointer[topic](data);
 }
+
+void handlePubishers() {
+        
+}
+
+
